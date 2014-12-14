2b8e94753db6607d2fcfe5cc6223435ab07557cd
marcuspoehls <marcus.poehls@gmail.com>
2014-12-14 14:05:17 +0100

    add initial template for user api

---
+++ server/api/v1/users.js
@@ -0,0 +1,44 @@
+var user            = require('../../models/user'),
+    users;
+
+
+/**
+*
+*/
+users = {
+    /**
+    *
+    */
+    find: function() {
+
+    },
+
+    /**
+    *
+    * @returns User
+    */
+    findById: function(id) {
+
+    },
+
+    /**
+     * @returns User
+     */
+    create: function() {
+
+    },
+
+    /**
+     * @returns User
+     */
+    update: function() {
+
+    }
+
+    /**
+     * @returns 
+     */
+    delete: function() {
+
+    }
+};
